e1888574ca80afa1a6e4f8a6cc79efef00624c0e
dizid <marc@dizid.com>
2019-10-31 18:19:37 +0100

    Firestore data loads AFTER computed values

--- src/components/ChartContainer.vue
+++ src/components/ChartContainer.vue
@@ -7,19 +7,28 @@
 <script>
 import { db } from '../db.js'
 export default {
-   name: 'BarChartContainer',
-   
+  // props: {twitter_screen_name: {type: String,required: false, default: 'dizid' }},
    data: () => ({ 
    tweetdata: [],  // These come from Firestore
-   screen_name: 'dizid',
+   twitter_screen_name: 'dizid',
    avgScores: {avgPdHappy:'',avgPdSad:'',avgPdAngry:'',avgPdFear:'',avgPdExcited:'',avgPdBored:'' }, 
-   loaded: false
+   componentLoaded: false
  }),
-// firestore:  {tweetdata: db.collection('tweets').where('screen_name', '==', screen_name)},
+
+// This works, but is loaded after the computed values are computed
+// firestore:  {tweetdata: db.collection('tweets').where('screen_name', '==', 'dizid')},
+
+ firestore() {
+    return {
+      tweetdata: db.collection('tweets').where('screen_name', '==', this.twitter_screen_name),
+    }
+  }, 
 
 computed: {
 calculateAvgScores() {
- //  if( this.tweetdata ) {
+ if(! this.componentLoaded) {
+            return null; }
+            else {
       this.avgScores.avgPdHappy = (this.tweetdata.map(tweetdata => tweetdata.pdHappy ).
          reduce((total, amount) => total + amount,0)/ +this.tweetdata.length * 100).toPrecision(2)
       this.avgScores.avgPdSad = (this.tweetdata.map(tweetdata => tweetdata.pdSad ).
@@ -35,18 +44,32 @@ calculateAvgScores() {
       this.loaded = true 
   //   }
 console.log("loaded: ", this.loaded)
-console.log("this.avgScores.avgPdExcited: 2", this.avgScores.avgPdExcited)
+console.log("this.avgScores.avgPdExcited: ", this.avgScores.avgPdExcited)
     return this.avgScores 
     }
+    }
 }, // end computed
-watch: {
-   tweetdata() {
-      // $bind automatically unbinds the previously bound property
-      this.$bind('tweetdata', db.collection('tweets').where('screen_name', '==', screen_name))
-    },
+
+/* watch: {
+    // Whenever the movie prop changes, fetch new data
+    twitter_screen_name(twitter_screen_name) {
+      // Fetch data about the tweets
+      this.$bind('tweetdata', db.collection('tweets').where('screen_name', '==', 'dizid')).then(tweetdata => {
+      this.tweetdata === tweetdata
+      });
+    }
+  } */
+
+/* created() {
+this.$bind('tweetdata', db.collection('tweets').where('screen_name', '==', 'dizid')).then(data => {
+  this.tweetdata === data
+})
+} */
+
+mounted() {
+   
+    this.componentLoaded = true;
   },
 
-
-
-}
+  }
 </script>
